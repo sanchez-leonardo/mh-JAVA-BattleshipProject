9532bf9d6afabf01eb85fade2d48cd58a3d71bf6
Leo Sanchez <sanchez.leo88@gmail.com>
2019-11-28 15:07:41 -0300

    Nueva configuracion para deployment

--- src/main/resources/static/server.js
+++ src/main/resources/static/server.js
@@ -1,8 +1,13 @@
+const history = require("connect-history-api-fallback");
 const express = require("expres");
+const path = require("path");
 const port = process.env.PORT || 8080;
-const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+const app = express();
+const staticFileMiddleware = expres.static();
+
+app.use(history());
+app.use(staticFileMiddleware);
 
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
