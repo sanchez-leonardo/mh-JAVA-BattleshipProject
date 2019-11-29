924c184fb340d7c4739dcb7bccc1cad350e75a6c
Leo Sanchez <sanchez.leo88@gmail.com>
2019-11-29 12:44:18 -0300

    Mas pruebas de dployment

--- src/main/java/com/comision5/salvo/restrepositories/SalvoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.comision5.salvo.restrepositories;
-
-import com.comision5.salvo.clases.Salvo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface SalvoRepository extends JpaRepository<Salvo, Long> {
-}
