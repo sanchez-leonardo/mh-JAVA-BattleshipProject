0831c4643ebacb913d8f67ccb04e32bbbf0d369e
Leo Sanchez <sanchez.leo88@gmail.com>
2019-12-01 16:56:08 -0300

    CORS una vez mas

--- src/main/java/com/comision5/salvo/SalvoApplication.java
+++ src/main/java/com/comision5/salvo/SalvoApplication.java
@@ -294,9 +294,9 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
       HttpServletResponse response = (HttpServletResponse) res;
       response.setHeader("Access-Control-Allow-Origin", "*");
-      response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+      response.setHeader("Access-Control-Allow-Methods", "POST, GET");
       response.setHeader("Access-Control-Max-Age", "3600");
-      response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+      response.setHeader("Access-Control-Allow-Headers", "Content-Type");
       chain.doFilter(req, res);
     }
   }
--- src/main/java/com/comision5/salvo/SalvoController.java
+++ src/main/java/com/comision5/salvo/SalvoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class SalvoController {
